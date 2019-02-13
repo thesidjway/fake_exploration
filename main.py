74ba244f737cc0a11167e3d4f52cdef39c323a70
Siddharth Jha <siddharthjha@outlook.com>
2019-02-13 12:56:48 +0530

    Basic RRT tree exploration added

--- main.py
+++ main.py
@@ -15,7 +15,9 @@ localization = FakeLocalization(50, 50, 10)  # starting position in x,y,z
 room = Room()
 lidar = Lidar()
 mapper = Mapping()
-planner = Planner()
+goal = Node(75, 25, 10)
+start = Node(50, 50, 10)
+planner = Planner(goal, start)
 
 fig = plt.figure()
 ax = fig.add_subplot(111,  projection='3d')
@@ -23,17 +25,18 @@ save_animation = False
 
 
 def update_plot(i):
-    # TODO: get location from planner
-    localization.update_position(random.randint(0, 99), random.randint(0, 99), random.randint(0, 99))
+    roommap = mapper.get_map()
+    botx, boty, botz = localization.get_curr_estimate()
+    newx, newy, newz = planner.iterate_planner(roommap, botx, boty, botz)
+    localization.update_position(newx, newy, newz)
     botx, boty, botz = localization.get_curr_estimate()
     xpt, ypt, zpt = lidar.sense(room, botx, boty, botz)
     mapper.update_map(xpt, ypt, zpt)
     roommap = mapper.get_map()
-    newx, newy, newz = planner.iterate_planner(roommap, botx, boty, botz)
-    localization.update_position(newx, newy, newz)
     x, y, z = roommap.nonzero()
     ax.clear()
     ax.scatter(x, y, z, zdir='z', c='b')
+    ax.scatter(botx, boty, botz, zdir='z', c='r')
 
 
 ani = animation.FuncAnimation(fig, update_plot, frames=np.arange(0, 50), interval=200)
--- planner.py
+++ planner.py
@@ -1,15 +1,67 @@
 import numpy as np
+import math
+import copy
+import random
 
+class Node():
+    def __init__(self, x, y, z):
+        self.x = x
+        self.y = y
+        self.z = z
+        self.parent = None
 
 class Planner(object):
-    def __init__(self):
-        self.goal = np.zeros((3))
+    def __init__(self, goal, start):
+        self.goal = goal
+        self.expandDis = 1.0
+        self.goalSampleRate = 10
+        self.maxIter = 500
+        self.nodeList = [start]
+
+    def GetNearestListIndex(self, nodeList, rnd):
+        dlist = [(node.x - rnd[0]) ** 2 + (node.y - rnd[1])
+                 ** 2 for node in nodeList]
+        minind = dlist.index(min(dlist))
+        return minind
 
     def iterate_planner(self, map, botx, boty, botz):
-        newx = botx
-        newy = boty
-        newz = botz
+
+        if random.randint(0, 100) > self.goalSampleRate:
+            rnd = [random.uniform(0, 100), random.uniform(0, 100)]
+        else:
+            rnd = [self.goal.x, self.goal.y]
+
+        nind = self.GetNearestListIndex(self.nodeList, rnd)
+
+        nearestNode = self.nodeList[nind]
+        theta = math.atan2(rnd[1] - nearestNode.y, rnd[0] - nearestNode.x)
+
+        newNode = copy.deepcopy(nearestNode)
+        newNode.x += self.expandDis * math.cos(theta)
+        newNode.y += self.expandDis * math.sin(theta)
+        newNode.z = botz
+        # newNode.parent = nind
+
+        self.nodeList.append(newNode)
+
+        newx = newNode.x
+        newy = newNode.y
+        newz= botz
+
+        if map[int(newx), int(newy), int(newz)] == 1:
+            return botx, boty, botz
+
+        # check goal
+        dx = newNode.x - self.goal.x
+        dy = newNode.y - self.goal.y
+        d = math.sqrt(dx * dx + dy * dy)
+        if d <= self.expandDis:
+            print("Goal!!")
+            return
+
         return newx, newy, newz
+
+
 
 
 
